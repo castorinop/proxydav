859bba8680d43ec40a7ae062aa5eadc8ac1d8a8e
Pablo Castorino <castorinop@gmail.com>
2012-11-04 13:29:39 -0300

    better passtrougth fix permissions

--- proxydav/web_ui.py
+++ proxydav/web_ui.py
@@ -11,9 +11,10 @@ import os.path
 import urllib
 import urllib2
 import urlparse
+import httplib
 
 from trac.core import *
-from trac.web.api import IRequestHandler, HTTPNotFound, HTTPForbidden
+from trac.web.api import Request, IRequestHandler, HTTPNotFound, HTTPForbidden
 from trac.mimeview.api import Context
 from trac.web.chrome import INavigationContributor, ITemplateProvider
 from trac.perm import IPermissionRequestor
@@ -44,13 +45,15 @@ class ProxyDavModule(Component):
         return req.path_info.startswith('/dav')
         
     def process_request(self, req):
+        CHUNK = 16 * 1024
+
         req.perm.assert_permission('PROXYDAV_PULL')
 
         # Check for no URL being configured
         if not self.proxydav_url:
             raise TracError('You must configure a URL in trac.ini')        
 
-        urlp = urlparse.urlparse(self.proxydav_url)
+        self.urlp = urlparse.urlparse(self.proxydav_url)
 
         self.log.info('check-path %r', req.path_info)
         if req.path_info.endswith('/dav'):
@@ -67,7 +70,7 @@ class ProxyDavModule(Component):
           if not req.perm.has_permission('PROXYDAV_PULL'):
             raise HTTPForbidden(_('dont have permission for pull repository'))
         else:
-          if not req.perm.has_permission('PROXDAV_PUSH'):
+          if not (req.perm.has_permission('PROXYDAV_PUSH') or req.perm.has_permission('PROXYDAV_ADMIN')):
             raise HTTPForbidden(_('dont have permission for push on this repository'))
 
 
@@ -79,10 +82,13 @@ class ProxyDavModule(Component):
         repo_path = path[len(path)-1]
         self.log.info('repo path  %r' % repo_path)
         repo_lbl  = self.get_repo_name(req)
-        repo_uris = '%s/%s' % (urlp.path, repo_path)
-        repo_urid = '%s/dav/%s' %(req.base_path, repo_lbl)
+        self.repo_host_git = self.urlp.hostname
 
-        self.log.info("from %r, to %r", repo_uris, repo_urid)
+        self.log.info('repo path  %r' % req.server_name)
+        self.repo_host_trac = req.server_name
+        self.repo_uri_git = '%s/%s' % (self.urlp.path, repo_path)
+        self.repo_uri_trac = '%s/dav/%s' %(req.base_path, repo_lbl)
+        self.log.info("git %r, trac %r", self.repo_uri_git, self.repo_uri_trac)
         
         urlf = None
         resp = 200
@@ -93,38 +99,79 @@ class ProxyDavModule(Component):
         #if req.environ['QUERY_STRING']:
         #print req.get_full_url()
        
-        suffix = self.removePrefix(req.path_info, '/dav');
-        suffix = suffix.replace(repo_lbl, repo_path);
-        self.log.info("req.path_info %r", suffix)
+        self.suffix = self.removePrefix(req.path_info, '/dav');
+        self.suffix = self.urlp.path + self.suffix.replace(repo_lbl, repo_path);
+        self.log.info("req.path_info %r", self.suffix)
 
-        #uri
-        uri = self.proxydav_url+suffix 
-      #  + '?' + req.environ['QUERY_STRING']
-        self.log.info("req-uri %r", uri)
+        self.log.info("req-uri %r", self.suffix)
 
-        r = urllib2.Request(uri)  
+        if req.method == 'GET' or req.method == 'PUT':
+          self.data_passtrougth(req)
+        else:
+          self.data_proxy(req)
 
-        '''method'''
+        self.log.info("====== done")
+        return
+
+        ''' Request To DAV '''
+        self.log.info("req-host %r", self.urlp.hostname)
+        rd = httplib.HTTPConnection(self.urlp.hostname)
+
         self.log.info("req-method %r", req.method)
-        r.get_method = lambda: req.method
+        rd.putrequest(req.method, self.repo_uri_git)
 
-        '''heads'''
+        '''headers from Request '''
         for k, v in req._inheaders:
           if k != 'x-forwarded-for' and k != 'x-real-ip':
-            v = v.replace(repo_urid, repo_uris)
-            r.add_header(k, v)
+            if v.find(self.repo_uri_trac):
+              v = v.replace(self.repo_uri_trac, self.repo_uri_git)
+              
+            rd.putheader(k, v)
             self.log.info("req-head %r => %r ", k,v)
+
+#        override lentgh  
+#        if req.get_header('Content-Length'):
+#          self.log.info("req-head content-length %r => %r ", int(req.get_header('Content-Length')), bitdiff)
+#          rd.putheader('content-length', int(req.get_header('Content-Length')) + bitdiff)
+
+        rd.endheaders()
+
+        ''' data from Request '''
+        while True:
+          chunk = req.read(CHUNK)
+          if not chunk: break
+          rd.send(chunk)
+
+        ''' Response from DAV '''
+        resp = rd.getresponse()
+                
+        ''' headers to Response '''
+        for k, v in head.items():
+          if isinstance(v, str):
+            v = v.replace(self.repo_uri_git, self.repo_uri_trac)
+          self.log.info("res-header %r, %r", k, v)
+          req.send_header(k, v)
+        
+        req.end_headers()
+
+        ''' data from Request '''
+        while True:
+          chunk = req.read(CHUNK)
+          if not chunk: break
+          req.write(chunk)
+
+        return
 
         ''' data '''
         data = req.read()
         #FIXME: only with PUT method ?
         if req.method == 'PUT':
           data = data.encode('string_escape')
-        self.log.info("req-data %r", data)
-        r.add_data(data)
+        #self.log.info("req-data %r", data)
+        rd.add_data(data)
 
         try: 
-          urlf = urllib2.urlopen(r) 
+          urlf = urllib2.urlopen(rd) 
           head = dict(urlf.info())
           page = urlf.read()
           if req.method != 'GET':
@@ -141,7 +188,7 @@ class ProxyDavModule(Component):
 
         #FIXME: needs rewrite paths
         if req.method != 'GET':
-          page = page.replace(repo_uris, repo_urid)
+          page = page.replace(self.repo_uri_git, self.repo_uri_trac)
 
  #       self.log.info("res-data %r", page)
 
@@ -157,7 +204,7 @@ class ProxyDavModule(Component):
           
         for k, v in head.items():
           if isinstance(v, str):
-            v = v.replace(repo_uris, repo_urid)
+            v = v.replace(self.repo_uri_git, self.repo_uri_trac)
           self.log.info("res-header %r, %r", k, v)
           req.send_header(k, v)
         
@@ -169,6 +216,144 @@ class ProxyDavModule(Component):
  	        page = page.encode('utf-8') 
 
         req.write(page)
+
+
+    def data_passtrougth(self, req):      
+      CHUNK = 16 * 1024
+      ''' Request To DAV '''
+      self.log.info("pass req-host %r", self.urlp.hostname)
+      dav = httplib.HTTPConnection(self.urlp.hostname)
+
+      self.log.info("pass req-method %r, %r", req.method, self.suffix)
+      dav.putrequest(req.method, self.suffix)
+
+      '''headers from Request '''
+      for k, v in req._inheaders:
+        if k != 'x-forwarded-for' and k != 'x-real-ip':
+          if v.find(self.repo_uri_trac):
+            v = v.replace(self.repo_uri_trac, self.repo_uri_git)
+            v = v.replace(self.repo_host_git, self.repo_host_trac)
+            
+          dav.putheader(k, v)
+          self.log.info("pass req-head %r => %r ", k,v)
+
+      #override lentgh  
+#      if req.get_header('Content-Length'):
+#        self.log.info("req-head content-length %r => %r ", int(req.get_header('Content-Length')), bitdiff)
+#        dav.putheader('content-length', int(req.get_header('Content-Length')) + bitdiff)
+
+      dav.endheaders()
+
+      ''' data from Request '''
+      while True:
+        chunk = req.read(CHUNK)
+        if not chunk: break
+        dav.send(chunk)
+
+      ''' Response from DAV '''
+      resp = dav.getresponse()
+
+      code = resp.status
+      self.log.info("pass resp %r ", code)
+      req.send_response(code)
+
+      ''' headers to Response '''
+      for k, v in resp.getheaders():
+        self.log.info("pass res-head-o %r, %r", k, v)
+        if isinstance(v, str):
+          v = v.replace(self.repo_uri_git, self.repo_uri_trac)
+          v = v.replace(self.repo_host_git, self.repo_host_trac)
+        self.log.info("pass res-head-f %r, %r", k, v)
+        req.send_header(k, v)
+
+      if not resp.getheader('content-length'):
+#        self.log.info("req-head content-length %r => %r ", int(req.get_header('Content-Length')), bitdiff)
+        req.send_header('content-length', 0)
+      
+      req.end_headers()
+
+      ''' data from Request '''
+      while True:
+        chunk = resp.read(CHUNK)
+        if not chunk: break
+        req.write(chunk)
+
+    def data_proxy(self, req):
+      ''' Request To DAV '''
+
+      self.log.info("prxy req-host %r", self.urlp.hostname)
+      dav = httplib.HTTPConnection(self.urlp.hostname)
+
+      self.log.info("prxy req-method %r, %r", req.method, self.suffix)
+      dav.putrequest(req.method, self.suffix)
+
+      page = req.read()
+      page = page.replace(self.repo_uri_trac, self.repo_uri_git)
+      page = page.replace(self.repo_host_trac, self.repo_host_git)
+
+      
+      '''headers from Request '''
+      for k, v in req._inheaders:
+        if k == 'content-length':
+          v = str(len(page))
+          self.log.info("prxy req-head content-length %r ", v)
+          dav.putheader('content-length', v)
+          continue;
+        if k != 'x-forwarded-for' and k != 'x-real-ip':
+          if v.find(self.repo_uri_trac):
+            v = v.replace(self.repo_uri_trac, self.repo_uri_git)
+            v = v.replace(self.repo_host_trac, self.repo_host_git)
+          
+          dav.putheader(k, v)
+          self.log.info("prxy req-head %r => %r ", k,v)
+
+      #override lentgh  
+#      if req.get_header('content-length'):
+#        v = str(len(page))
+#        self.log.info("prxy req-head content-length %r => %r ", v)
+#        dav.putheader('content-length', v)
+
+      dav.endheaders()
+
+      ''' data from Request '''
+      dav.send(page)
+
+      #self.log.info("prxy send %r ", page)
+      ''' Response from DAV '''
+      resp = dav.getresponse()
+
+      code = resp.status
+      page = resp.read()
+      page = page.replace(self.repo_uri_git, self.repo_uri_trac)
+      page = page.replace(self.repo_host_git, self.repo_host_trac)
+
+
+      self.log.info("prxy resp %r ", code)
+      #self.log.info("prxy resp %r ", page)
+      
+      if isinstance(page, unicode): 
+ 	        page = page.encode('utf-8') 
+   
+      req.send_response(code)
+      ''' headers to Response '''
+      for k, v in resp.getheaders():
+        if k == "content-length":
+          continue;
+        if isinstance(v, str):
+          v = v.replace(self.repo_uri_git, self.repo_uri_trac)
+          v = v.replace(self.repo_host_git, self.repo_host_trac)
+        self.log.info("prxy res-header %r, %r", k, v)
+        req.send_header(k, v)
+ #     if resp.getheader('content-length'):
+      self.log.info("prxy res-head content-length %r ", len(page))
+      req.send_header('content-length', len(page))
+
+      
+      req.end_headers()
+
+      ''' data from Request '''
+      req.write(page)
+
 
     def help(self, req):
 
